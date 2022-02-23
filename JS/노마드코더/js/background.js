963d11a97dc2908111b38f82980a2422456f91a4
YooJongHyeon <justin77748292@gmail.com>
2022-02-23 23:19:03 +0900

    노마드 CSS1일차

--- JS/노마드코더/js/background.js
+++ JS/노마드코더/js/background.js
@@ -5,5 +5,6 @@ const chosenImage = images[Math.floor(Math.random() * images.length)]; //0~ë°°ì—
 const bgImage = document.createElement("img");
 
 bgImage.src = `img/${chosenImage}`;
+bgImage.id = "bg-image";
 
 document.body.appendChild(bgImage);
